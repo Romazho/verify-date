9822746d4a40708ae77a19f74f965d0d76401bc6
Roman <Romazho123@gmail.com>
2020-02-07 21:02:26 -0500

    Finish the projet verification date with succes
    Finished the project verification date with success

--- NextDate/verifyDate.py
+++ NextDate/verifyDate.py
@@ -1,32 +1,55 @@
 
-print("Hello and welcome to The Date Verification App!")
+def verifyDate():
+    print("Hello and welcome to The Date Verification App!")
 
-dayIsCorrect = False
+    dayIsCorrect = False
 
-while (not dayIsCorrect):
-    print("Please enter a day: ")
-    day = int(input())
-    if (day < 1 or day > 31):
-        print("Attention! The day must be between 1 and 31.")
-    else:
-        dayIsCorrect = True
+    while (not dayIsCorrect):
+        print("Please enter a day: ")
+        day = int(input())
+        if (day < 1 or day > 31):
+            print("Attention! The day must be between 1 and 31.")
+        else:
+            dayIsCorrect = True
+
+    oddMonths = [1, 3, 5, 7, 8, 10, 12]  # months that have 31 days
+    evenMonths = [4, 6, 9, 11]  # months that have 30 days
+    febuary = 2
+
+    monthIsCorrect = False
+
+    while (not monthIsCorrect):
+        print("Please enter a month: ")
+        month = int(input())
+        if (month < 1 or month > 12):
+            print("Attention! The month must be between 1 and 12.")
+        elif ((month in evenMonths) and day == 31):
+            print("Attention! This month can't have 31 days")
+        elif ((month == 2) and day > 29):
+            print("Attention! Febuary can't have more than 29 days")
+        else:
+            monthIsCorrect = True
+
+    yearIsCorrect = False
+
+    while (not yearIsCorrect):
+        print("Please enter a year: ")
+        year = int(input())
+        if (year < 0):
+            print("Attention! A year cannot be negative.")
+        elif(year % 1000 == 0 and month == febuary):
+            yearIsCorrect = True
+        elif(year % 100 == 0 and month == febuary and day > 28):
+            print("Attention! The day " + str(day) + " cannot exist on the month of Febuary on the year " +
+                  str(year) + ". Because a 'hundred' year isn't actually a leap year!")
+        elif(year % 4 != 0 and month == febuary and day > 28):
+            print("Attention! The day " + str(day) + " cannot exist on the month of Febuary on the year " +
+                  str(year) + ". Because it's not a leap year!")
+        else:
+            yearIsCorrect = True
+
+    print("Congragulation! You have chosen a correct date! Your date is: " +
+          str(day) + "/" + str(month) + "/" + str(year))
 
 
-monthIsCorrect = False
-oddMonths = [1, 3, 5, 7, 8, 10, 12]  # months that have 31 days
-evenMonths = [4, 6, 9, 11]  # months that have 30 days
-febuary = 2
-
-while (not monthIsCorrect):
-    print("Please enter a month: ")
-    month = int(input())
-    if (month < 1 or month > 12):
-        print("Attention! The month must be between 1 and 12.")
-    elif ((month in evenMonths) and day == 31):
-        print("Attention! This month can't have 31 days")
-    elif ((month == 2) and day > 29):
-        print("Attention! Febuary can't have more than 29 days")
-    else:
-        monthIsCorrect = True
-
-yearIsCorrect = False
+verifyDate()
